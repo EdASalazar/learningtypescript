c51dbd22f207f8f73aa84079aee30fbbbc185007
EdASalazar <edasalazar123@gmail.com>
2023-04-19 10:42:32 -0700

    Finished lesson

--- src/detection.ts
+++ src/detection.ts
@@ -58,3 +58,54 @@ function getFood(pet: Fish | Bird) {
   }
 }
 
+// discrimianted union and exhaustive checking
+
+// hacky way according to vidoe
+// interface Circle {
+//   kind: "circle"
+// }
+
+// interface Square {
+//   kind: "Square"
+// }
+
+// not hacky way
+
+interface Circle {
+  kind: "circle",
+  radius: number
+}
+
+interface Square {
+  kind: "square",
+  side: number
+}
+
+interface Rectangle {
+  kind: "rectangle",
+  length: number,
+  width: number
+}
+
+type Shape = Circle | Square | Rectangle
+
+function getTrueShape(shape: Shape) {
+  if (shape.kind === "circle") {
+    return Math.PI * shape.radius ** 2
+  }
+  // return shape.side * shape.side
+}
+
+function getArea(shape: Shape) {
+  switch(shape.kind){
+    case "circle":
+      return Math.PI * shape.radius ** 2
+    case "square":
+      return shape.side * shape.side
+    case "rectangle":
+      return shape.length * shape.width 
+    default:
+      const _defaultforshape: never = shape
+      return _defaultforshape
+  }
+}
