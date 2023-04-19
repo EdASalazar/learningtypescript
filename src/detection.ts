fa330d333d1ce3b5ebb013f6060cd2a74f0ad54a
EdASalazar <edasalazar123@gmail.com>
2023-04-19 10:04:01 -0700

    Type narrowing

--- src/detection.ts
+++ src/detection.ts
@@ -1,12 +1,12 @@
-function detectType(val: number | string ) {
+function detectType(val: number | string) {
   if (typeof val === 'string') {
     return val.toLocaleLowerCase()
   }
   return val + 3
 }
 
-function provideId(id: string | null){
-  if(!id) {
+function provideId(id: string | null) {
+  if (!id) {
     console.log('Please provide ID')
     return
   }
@@ -26,8 +26,35 @@ interface Admin {
 
 
 function isAdminAccount(account: User | Admin) {
-  if("isAdmin" in account) {
+  if ("isAdmin" in account) {
     return account.isAdmin
   }
- 
+
 }
+
+
+function logValue(x: Date | string) {
+  if (x instanceof Date) {
+    console.log(x.toUTCString());
+  } else {
+    console.log(x.toLocaleUpperCase());
+  }
+}
+
+type Fish = {swim: () => void};
+type Bird = {fly: () => void};
+
+function isFish(pet: Fish | Bird): pet is Fish {
+  return (pet as Fish).swim !== undefined;
+}
+
+function getFood(pet: Fish | Bird) {
+  if (isFish(pet)) {
+    pet
+    return "fish food"
+  } else {
+    pet
+    return "bird food"
+  }
+}
+
